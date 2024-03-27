27c6118eeb95b60ab3bdf2e9fddcafd9ee45f434
VIVARND <vivarnd77@gmail.com>
2024-03-27 11:08:32 +0900

    Update seed_start.py

--- seed_start.py
+++ seed_start.py
@@ -1,7 +1,6 @@
 from modules.drone_sitl import DroneSitl 
 from modules.my_drone import MyDrone
 
-
 def sitl_start():
     drone_sitl = DroneSitl()
     start(drone_sitl.sitl.connection_string())
@@ -18,4 +17,5 @@ def start(connection_string):
     my_drone = MyDrone(connection_string)
     my_drone.seed_start()
 
-real_start()
+# 시뮬레이터를 사용하여 드론 시뮬레이션 실행
+sitl_start()
